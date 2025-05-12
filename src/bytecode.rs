5bd9903ae1af21d95389c38051c9d24c1c960133
Hugo Guerrier <guerrier@adacore.com>
2025-05-12 23:12:26 +0200

    Add the 'bytecode' module to hold information about LuaJIT bytecode

---
+++ src/bytecode.rs
@@ -0,0 +1,246 @@
+//! # LuaJIT bytecode module
+//!
+//! This module contains all required information and data to produce valid
+//! LuaJIT bytecode buffers (version 2).
+//!
+//! A LuaJIT bytecode buffer has the following structure:
+//!   * A header containing information about the execution context
+//!   * Multiple function prototypes
+//!   * One byte set to `0` to flag the buffer end
+//!
+//! The last prototype is considered as the "main" function and is
+//! automatically executed, it must be variadic.
+//!
+//! ## The header part
+//!
+//! A LuaJIT bytecode buffer header part is defined like this:
+//!
+//! ```no_run
+//! MAGIC (3 byte) = [0x1B, 0x4C, 0x4A]
+//! VERSION (1 byte) = 0x01 | 0x02
+//! FLAGS (1 byte) = {
+//!     0b00000001 = IS_BIG_ENDIAN
+//!     0b00000010 = IS_STRIPPED
+//!     0b00000100 = HAS_FFI_ACCESS
+//!     0b00001000 = ???
+//! }
+//! (
+//!     SOURCE_NAME_LEN (1 uleb128)
+//!     SOURCE_NAME (byte[SOURCE_NAME_LEN])
+//! )? if !IS_STRIPPED
+//! ```
+//!
+//! ## Prototypes
+//!
+//! Here is the structure of a function prototype in LuaJIT bytecode format:
+//!
+//! ```no_run
+//! SIZE (1 uleb128)
+//! FLAGS (1 byte) = {
+//!     0b00000001 = HAS_CHILD
+//!     0b00000010 = IS_VARIADIC
+//!     0b00000100 = HAS_FFI
+//!     0b00001000 = JIT_DISABLED
+//!     0b00010000 = HAS_ILOOP
+//! }
+//! ARG_COUNT (1 byte)
+//! FRAME_SIZE (1 byte)
+//! UPVALUE_CONST_COUNT (1 byte)
+//! COMPLEX_CONST_COUNT (1 uleb128)
+//! NUM_CONST_COUNT (1 uleb128)
+//! INST_COUNT (1 uleb128)
+//! (
+//!     DEBUG_INFO_SIZE (1 uleb128)
+//!     FIRST_LINE_NB (1 uleb128)
+//!     LINE_COUNT (1 uleb128)
+//! )? if !IS_STRIPPED
+//! INSTRUCTIONS (1 int32[INST_COUNT])
+//! CONSTANT_TABLE
+//! ```
+//!
+//! With:
+//!   * `SIZE` -> The size of the whole function prototype, without the `SIZE`
+//!     field
+//!   * `FRAME_SIZE` -> The number of local slots used during the function
+//!     execution
+//!
+//! When a function is called, arguments are placed in first slots of the
+//! frame, following their declaration order. Example:
+//!
+//! ```lua
+//! function test(a, b) end
+//! ```
+//!
+//! In the bytecode representation of the previous Lua code, `a` variable is
+//! stored in the local slot `0` and `b` in the `1`.
+//!
+//! ## The constant table
+//!
+//! `CONSTANT_TABLE` is a section at the end of every function prototype which
+//! contains all constants used in the function. Its structure is:
+//!
+//! ### Up-value constants
+//!
+//! ```no_run
+//! UPVALUE_CONSTS (1 short16[UPVAL_COUNT])
+//! COMPLEX_CONSTS (1 COMPLEX_CONST[COMPLEX_CONST_COUNT])
+//! NUMERIC_CONSTS (1 NUMERIC_CONST[NUM_CONST_COUNT])
+//! ```
+//!
+//! An "up-value" (shortened "UV") is an index that correspond to a local slot
+//! or an UV constant in the parent scope that is referenced in the function.
+//! This is used to encode the "closure" concept in LuaJIT.
+//! An UV constant is a 2-byte value representing an index and a flag. The two
+//! highest bits tells whether the index is about a local variable or another
+//! UV in the parent scope. Example:
+//!   * `0b1100000000000001` -> Enclose the local slot in the parent scope with
+//!     index "1"
+//!   * `0b0000000000000001` -> Enclose the UV constant in the parent scope
+//!     with index "1"
+//!
+//! ### Complex constants
+//!
+//! A `COMPLEX_CONST` in the LuaJIT bytecode is composed of two parts:
+//!
+//! ```no_run
+//! CONSTANT_KIND (1 uleb128) = {
+//!     0 = CHILD
+//!     1 = TABLE
+//!     2 = I64
+//!     3 = U64
+//!     4 = COMPLEX
+//!     5 + STR_LEN = STR
+//! }
+//! CONSTANT_DATA (~ depending on CONSTANT_KIND)
+//! ```
+//!
+//! With:
+//!   * `CHILD` -> A function prototype, child of the current prototype
+//!
+//! #### Table constants
+//!
+//! Table constants are structured as specified bellow:
+//!
+//! ```no_run
+//! ARRAY_ITEM_COUNT (1 uleb128)
+//! HASH_ITEM_COUNT (1 uleb128)
+//! ARRAY (TABLE_CONSTANT[ARRAY_ITEM_COUNT])
+//! MAP ((TABLE_CONSTANT, TABLE_CONSTANT)[HASH_ITEM_COUNT])
+//! ```
+//!
+//! **IMPORTANT:** Since Lua table are 1-indexed, if a table constant owns array
+//! items, `nil` value inserted at the first position and counted in the
+//! `ARRAY_ITEM_COUNT` value.
+//!
+//! A `TABLE_CONSTANT` is a value that follow the same principle as
+//! `COMPLEX_CONSTANT`: an ULEB128 as kind with a constant data which size
+//! is depending of the type, as the following description shows:
+//!
+//! ```no_run
+//! TABLE_CONSTANT_KIND (1 uleb128) = {
+//!     0 = nil
+//!     1 = false
+//!     2 = true
+//!     3 = INTEGER
+//!     4 = NUM
+//!     5 = STRING
+//! }
+//! TABLE_CONSTANT_DATA (~ depending on TABLE_CONSTANT_KIND)
+//! ```
+//!
+//! Here are the encoding of `TABLE_CONSTANT_DATA` in function of the
+//! associated `TABLE_CONSTANT_KIND`:
+//!   * If `TABLE_CONSTANT_KIND` is `0`, `1` or `2` -> There is no
+//!     `TABLE_CONSTANT_DATA` field
+//!   * If `TABLE_CONSTANT_KIND` is `3` -> The data is a ULEB128 representing
+//!     the value of the i32 encoded integer
+//!   * If `TABLE_CONSTANT_KIND` is `4` -> The data field is a float64 encoded
+//!     as 2 ULEB128 numbers, first one being the low part of the number (32
+//!     lease significant bits), second one representing the high part (32
+//!     other bits
+//!   * If `TABLE_CONSTANT_KIND` is `5` or more -> The data represents a string
+//!     and follows the same rules as [string constants](#string-constants)
+//!
+//! #### Integer constants
+//!
+//! I64 and U64 constants represents simple integers, they are encoded in the
+//! ULEB128 format.
+//!
+//! #### Complex constants
+//!
+//! **TODO**
+//!
+//! #### String constants
+//!
+//! A string constant is identified by a kind greater or equals to 5. Since the
+//! kind is an ULEB128 value, it also contains the length of the following
+//! string. Example:
+//!   * `0x08 0x66 0x6F 0x6F` -> Represents the string constant "foo"
+//!
+//! ### Numeric constants
+//!
+//! In the LuaJIT bytecode format, a numeric constant may represent either an
+//! integer or a number value. A number may be a decimal value, thus we need to
+//! represents this decimal part.
+//!
+//! In order to encode those possibilities, in the first element of the ULEB128
+//! representation of the value, the least significant bit tells whether the
+//! numeric constant is an integer or a float value. Example:
+//!   * `0b00000010` -> Means that the value is an integer valued to "1"
+//!     (`0000001` == `1`)
+//!   * `0b00000011 0b00000101` -> Means that the value is a decimal with the
+//!     low part first, then the high part
+//!
+//! Integer values are represented using signed 32 bits integers, if the
+//! constant value cannot be represented in it, it becomes a float64 decimal
+//! constant.
+//!
+//! Decimal numbers are represented using float64 encoded values, their
+//! representation in the bytecode buffer is done as described bellow:
+//!
+//! ```no_run
+//! LOW_PART = (1 uleb128)  // The 32 least significant bits of the f64 with
+//!                         // the numeric constant flag on the first byte
+//! HIGH_PART = (1 uleb128) // The 32 most significant bits of the f64
+//! ```
+
+mod op_codes;
+
+// ----- Header related constants -----
+
+pub const MAGIC: [u8; 3] = [0x1B, 0x4C, 0x4A];
+pub const CUR_VERSION: u8 = 0x02;
+pub const MAX_VERSION: u8 = 0x80;
+
+pub const FLAG_H_IS_BIG_ENDIAN: u8 = 0b00000001; // Whether the bytecode is encoded in big endian
+pub const FLAG_H_IS_STRIPPED: u8 = 0b00000010; // Whether the the bytecode is stripped (without debug info)
+pub const FLAG_H_HAS_FFI: u8 = 0b00000100; // Whether the bytecode has FFI access
+pub const FLAG_H_FR2: u8 = 0b00001000; // ???
+
+// ----- Prototype related constants -----
+
+pub const FLAG_P_HAS_CHILD: u8 = 0b00000001;
+pub const FLAG_P_IS_VARIADIC: u8 = 0b00000010;
+pub const FLAG_P_HAS_FFI: u8 = 0b00000100;
+pub const FLAG_P_JIT_DISABLED: u8 = 0b00001000;
+pub const FLAG_P_HAS_ILOOP: u8 = 0b00010000;
+
+pub const JUMP_BIASING: u16 = 0x8000;
+
+// ----- Complex constant kinds -----
+
+pub const COMPLEX_CONST_CHILD_KIND: u8 = 0;
+pub const COMPLEX_CONST_TAB_KIND: u8 = 1;
+pub const COMPLEX_CONST_I64_KIND: u8 = 2;
+pub const COMPLEX_CONST_U64_KIND: u8 = 3;
+pub const COMPLEX_CONST_COMPLEX_NUM_KIND: u8 = 4;
+pub const COMPLEX_CONST_STR_KIND: u8 = 5;
+
+// ----- Table constant kinds -----
+
+pub const TABLE_CONST_NIL_KIND: u8 = 0;
+pub const TABLE_CONST_FALSE_KIND: u8 = 1;
+pub const TABLE_CONST_TRUE_KIND: u8 = 2;
+pub const TABLE_CONST_INT_KIND: u8 = 3;
+pub const TABLE_CONST_NUM_KIND: u8 = 4;
+pub const TABLE_CONST_STR_KIND: u8 = 5;
---
+++ src/bytecode/op_codes.rs
@@ -0,0 +1,755 @@
+#![allow(unused)]
+
+//! # Bytecode instructions
+//!
+//! This module contains all available LuaJIT bytecode operations codes, with
+//! a comprehensive documentation about their format and semantics.
+//! In the LuaJIT bytecode format, an instruction is always the same size:
+//! 32bits. An instruction structure may be one of the two following:
+//!
+//! ```no_run
+//! B (1 byte)
+//! C (1 byte)
+//! A (1 byte)
+//! OP (1 byte)
+//! ```
+//!
+//! Or
+//!
+//! ```no_run
+//! D (2 bytes)
+//! A (1 byte)
+//! OP (1 byte)
+//! ```
+//!
+//! With:
+//!   * `A`, `B`, `C` and `D` -> Operands
+//!   * `OP` -> Operation code
+//!
+//! The kind of an instruction is defined by its operation code.
+//! Instructions operands may have on of the following types:
+//!   * slot -> Local variable slot index
+//!   * rslot -> Local variable slot index (read-only)
+//!   * uv: Up-value index
+//!   * lit: Literal
+//!   * slit: Signed literal
+//!   * pri: Primitive value (0 = nil, 1 = false, 2 = true)
+//!   * num: Number constant, index into constant table
+//!   * string: String constant, index into constant table
+//!   * tab: Template table, index into constant table
+//!   * func: Function prototype, index into constant table
+//!   * cdata: Complex constant, index into constant table
+//!   * jump: Branch target, relative to next instruction, biased with 0x8000
+//!
+//! Additionally to operands, the `MULTRES` variable may be accessed by
+//! some operations to perform computing. `MULTRES` is an internal variable
+//! that keeps track of the number of results returned by the previous call or
+//! by `VARG` operation.
+
+// ----- Variable comparison operations -----
+
+/// Jump if A < D (Always followed by a [`JMP`] instruction holding the
+/// branching target).
+/// ```no_run
+/// A: slot
+/// D: slot
+/// ```
+pub const ISLT: u8 = 0x00;
+
+/// Jump if A >= D (Always followed by a [`JMP`] instruction holding the
+/// branching target).
+/// ```no_run
+/// A: slot
+/// D: slot
+/// ```
+pub const ISGE: u8 = 0x01;
+
+/// Jump if A <= D (Always followed by a [`JMP`] instruction holding the
+/// branching target).
+/// ```no_run
+/// A: slot
+/// D: slot
+/// ```
+pub const ISLE: u8 = 0x02;
+
+/// Jump if A > D (Always followed by a [`JMP`] instruction holding the
+/// branching target).
+/// ```no_run
+/// A: slot
+/// D: slot
+/// ```
+pub const ISGT: u8 = 0x03;
+
+/// Jump if A == D (Always followed by a [`JMP`] instruction holding the
+/// branching target).
+/// ```no_run
+/// A: slot
+/// D: slot
+/// ```
+pub const ISEQV: u8 = 0x04;
+
+/// Jump if A != D (Always followed by a [`JMP`] instruction holding the
+/// branching target).
+/// ```no_run
+/// A: slot
+/// D: slot
+/// ```
+pub const ISNEV: u8 = 0x05;
+
+// ----- String constant comparison operations -----
+
+/// Jump if A == D (Always followed by a [`JMP`] instruction holding the
+/// branching target).
+/// ```no_run
+/// A: slot
+/// D: string
+/// ```
+pub const ISEQS: u8 = 0x06;
+
+/// Jump if A != D (Always followed by a [`JMP`] instruction holding the
+/// branching target).
+/// ```no_run
+/// A: slot
+/// D: string
+/// ```
+pub const ISNES: u8 = 0x07;
+
+// ----- Numeric constant comparison operations -----
+
+/// Jump if A == D (Always followed by a [`JMP`] instruction holding the
+/// branching target).
+/// ```no_run
+/// A: slot
+/// D: num
+/// ```
+pub const ISEQN: u8 = 0x08;
+
+/// Jump if A != D (Always followed by a [`JMP`] instruction holding the
+/// branching target).
+/// ```no_run
+/// A: slot
+/// D: num
+/// ```
+pub const ISNEN: u8 = 0x09;
+
+// ----- Primitive comparison operations -----
+
+/// Jump if A == D (Always followed by a [`JMP`] instruction holding the
+/// branching target).
+/// ```no_run
+/// A: slot
+/// D: pri
+/// ```
+pub const ISEQP: u8 = 0x0A;
+
+/// Jump if A != D (Always followed by a [`JMP`] instruction holding the
+/// branching target)/
+/// ```no_run
+/// A: slot
+/// D: pri
+/// ```
+pub const ISNEP: u8 = 0x0B;
+
+// ----- Unary copy and test operations -----
+
+/// Copy D to A and jump, if D is true (Always followed by a [`JMP`]
+/// instruction holding the branching target).
+/// ```no_run
+/// A: slot
+/// D: slot
+/// ```
+pub const ISTC: u8 = 0x0C;
+
+/// Copy D to A and jump, if D is false (Always followed by a [`JMP`]
+/// instruction holding the branching target).
+/// ```no_run
+/// A: slot
+/// D: slot
+/// ```
+pub const ISFC: u8 = 0x0D;
+
+// ----- Unary test operations -----
+
+/// Jump D is true (Always followed by a [`JMP`] instruction holding the
+/// branching target).
+/// ```no_run
+/// D: slot
+/// ```
+pub const IST: u8 = 0x0E;
+
+/// Jump D is false (Always followed by a [`JMP`] instruction holding the
+/// branching target).
+/// ```no_run
+/// D: slot
+/// ```
+pub const ISF: u8 = 0x0F;
+
+/// TODO
+pub const ISTYPE: u8 = 0x10;
+
+/// TODO
+pub const ISNUM: u8 = 0x11;
+
+// ----- Copying operations -----
+
+/// Copy D to A.
+/// ```no_run
+/// A: slot
+/// D: slot
+/// ```
+pub const MOV: u8 = 0x12;
+
+// ----- Unary operations -----
+
+/// Set A to !D.
+/// ```no_run
+/// A: slot
+/// D: slot
+/// ```
+pub const NOT: u8 = 0x13;
+
+/// Set A to -D.
+/// ```no_run
+/// A: slot
+/// D: slot
+/// ```
+pub const UNM: u8 = 0x14;
+
+/// Set A to #D (length of D).
+/// ```no_run
+/// A: slot
+/// D: slot
+/// ```
+pub const LEN: u8 = 0x15;
+
+// ----- Numeric constant binary operations -----
+
+/// Set A to B + C.
+/// ```no_run
+/// A: slot
+/// B: slot
+/// C: num
+/// ```
+pub const ADDVN: u8 = 0x16;
+
+/// Set A to B - C.
+/// ```no_run
+/// A: slot
+/// B: slot
+/// C: num
+/// ```
+pub const SUBVN: u8 = 0x17;
+
+/// Set A to B * C.
+/// ```no_run
+/// A: slot
+/// B: slot
+/// C: num
+/// ```
+pub const MULVN: u8 = 0x18;
+
+/// Set A to B / C.
+/// ```no_run
+/// A: slot
+/// B: slot
+/// C: num
+/// ```
+pub const DIVVN: u8 = 0x19;
+
+/// Set A to B % C.
+/// ```no_run
+/// A: slot
+/// B: slot
+/// C: num
+/// ```
+pub const MODVN: u8 = 0x1A;
+
+// ----- Reversed numeric constant binary operations -----
+
+/// Set A to C + B.
+/// ```no_run
+/// A: slot
+/// B: slot
+/// C: num
+/// ```
+pub const ADDNV: u8 = 0x1B;
+
+/// Set A to C - B.
+/// ```no_run
+/// A: slot
+/// B: slot
+/// C: num
+/// ```
+pub const SUBNV: u8 = 0x1C;
+
+/// Set A to C * B.
+/// ```no_run
+/// A: slot
+/// B: slot
+/// C: num
+/// ```
+pub const MULNV: u8 = 0x1D;
+
+/// Set A to C / B.
+/// ```no_run
+/// A: slot
+/// B: slot
+/// C: num
+/// ```
+pub const DIVNV: u8 = 0x1E;
+
+/// Set A to C % B.
+/// ```no_run
+/// A: slot
+/// B: slot
+/// C: num
+/// ```
+pub const MODNV: u8 = 0x1F;
+
+// ----- Variable binary operations -----
+
+/// Set A to B + C.
+/// ```no_run
+/// A: slot
+/// B: slot
+/// C: slot
+/// ```
+pub const ADDVV: u8 = 0x20;
+
+/// Set A to B - C.
+/// ```no_run
+/// A: slot
+/// B: slot
+/// C: slot
+/// ```
+pub const SUBVV: u8 = 0x21;
+
+/// Set A to B * C.
+/// ```no_run
+/// A: slot
+/// B: slot
+/// C: slot
+/// ```
+pub const MULVV: u8 = 0x22;
+
+/// Set A to B / C.
+/// ```no_run
+/// A: slot
+/// B: slot
+/// C: slot
+/// ```
+pub const DIVVV: u8 = 0x23;
+
+/// Set A to B % C.
+/// ```no_run
+/// A: slot
+/// B: slot
+/// C: slot
+/// ```
+pub const MODVV: u8 = 0x24;
+
+/// Set A to B ^ C (B power C).
+/// ```no_run
+/// A: slot
+/// B: slot
+/// C: slot
+/// ```
+pub const POW: u8 = 0x25;
+
+/// Set A to the result of the concatenation of all values from B to C (both
+/// inclusive).
+/// ```no_run
+/// A: slot
+/// B: slot
+/// C: slot
+/// ```
+pub const CAT: u8 = 0x26;
+
+// ----- Constant loading operations -----
+
+/// Set A to string constant D.
+/// ```no_run
+/// A: slot
+/// D: string
+/// ```
+pub const KSTR: u8 = 0x27;
+
+/// Set A to complex constant D.
+/// ```no_run
+/// A: slot
+/// D: cdata
+/// ```
+pub const KCDATA: u8 = 0x28;
+
+/// Set A to signed short D.
+/// ```no_run
+/// A: slot
+/// D: slit
+/// ```
+pub const KSHORT: u8 = 0x29;
+
+/// Set A to numeric constant D.
+/// ```no_run
+/// A: slot
+/// D: num
+/// ```
+pub const KNUM: u8 = 0x2A;
+
+/// Set A to primitive value D.
+/// ```no_run
+/// A: slot
+/// D: pri
+/// ```
+pub const KPRI: u8 = 0x2B;
+
+/// Set slots from A to D to nil.
+/// ```no_run
+/// A: slot
+/// D: slot
+/// ```
+pub const KNIL: u8 = 0x2C;
+
+// ----- Up-value operations -----
+
+/// Set A to up-value D.
+/// ```no_run
+/// A: slot
+/// D: uv
+/// ```
+pub const UGET: u8 = 0x2D;
+
+/// Set up-value A to D.
+/// ```no_run
+/// A: uv
+/// D: slot
+/// ```
+pub const USETV: u8 = 0x2E;
+
+/// Set up-value A to string constant D.
+/// ```no_run
+/// A: uv
+/// D: string
+/// ```
+pub const USETS: u8 = 0x2F;
+
+/// Set up-value A to numeric constant D.
+/// ```no_run
+/// A: uv
+/// D: num
+/// ```
+pub const USETN: u8 = 0x30;
+
+/// Set up-value A to primitive value D.
+/// ```no_run
+/// A: uv
+/// D: pri
+/// ```
+pub const USETP: u8 = 0x31;
+
+/// Close up-values for slots >= A and jump to D.
+/// ```no_run
+/// A: rslot
+/// D: jump
+/// ```
+pub const UCLO: u8 = 0x32;
+
+// ----- Function operations -----
+
+/// Create new closure from prototype D and store it in A.
+/// ```no_run
+/// A: slot
+/// D: func
+/// ```
+pub const FNEW: u8 = 0x33;
+
+// ----- Table creation operations -----
+
+/// Set A to new table with size D.
+///
+/// D is split into two fields:
+///   * 11 lowest bits give the array part size as unsigned int
+///   * 5 highest bits give the hash part size as a power of 2
+///
+/// ```no_run
+/// A: slot
+/// D: lit
+/// ```
+pub const TNEW: u8 = 0x34;
+
+/// Duplicate table D and set the result to A.
+/// ```no_run
+/// A: slot
+/// D: tab
+/// ```
+pub const TDUP: u8 = 0x35;
+
+// ----- Global table operations -----
+
+/// Set A to global_table[D].
+/// ```no_run
+/// A: slot
+/// D: string
+/// ```
+pub const GGET: u8 = 0x36;
+
+/// Set global_table[D] to A.
+/// ```no_run
+/// A: slot
+/// D: string
+/// ```
+pub const GSET: u8 = 0x37;
+
+// ----- Table fetching operations -----
+
+/// Set A to B[C].
+/// ```no_run
+/// A: slot
+/// B: slot
+/// C: slot
+/// ```
+pub const TGETV: u8 = 0x38;
+
+/// Set A to B[C].
+/// ```no_run
+/// A: slot
+/// B: slot
+/// C: string
+/// ```
+pub const TGETS: u8 = 0x39;
+
+/// Set A to B[C].
+/// ```no_run
+/// A: slot
+/// B: slot
+/// C: lit
+/// ```
+pub const TGETB: u8 = 0x3A;
+
+/// TODO
+pub const TGETR: u8 = 0x3B;
+
+// ----- Table setting operations -----
+
+/// Set B[C] to A.
+/// ```no_run
+/// A: slot
+/// B: slot
+/// C: slot
+/// ```
+pub const TSETV: u8 = 0x3C;
+
+/// Set B[C] to A.
+/// ```no_run
+/// A: slot
+/// B: slot
+/// C: string
+/// ```
+pub const TSETS: u8 = 0x3D;
+
+/// Set B[C] to A.
+/// ```no_run
+/// A: slot
+/// B: slot
+/// C: lit
+/// ```
+pub const TSETB: u8 = 0x3E;
+
+/// Set A-1[D],A-1[D+1]...A-1[D+MULTRES] = A,A+1...A+MULTRES.
+/// D must be a floating point constant and only the 32 lowest bits of the
+/// mantissa is going to be used as an index.
+/// ```no_run
+/// A: slot
+/// D: num
+/// ```
+pub const TSETM: u8 = 0x3F;
+
+/// TODO
+pub const TSETR: u8 = 0x40;
+
+// ----- Function calling operations -----
+
+/// Call A(A+2...A+C+MULTRES) and set A,A+1...A+B-2 to returned values.
+/// ```no_run
+/// A: slot
+/// B: lit
+/// C: lit
+/// ```
+pub const CALLM: u8 = 0x41;
+
+/// Call A(A+2...A+C-1) and set A,A+1...A+B-2 to returned values.
+/// ```no_run
+/// A: slot
+/// B: lit
+/// C: lit
+/// ```
+pub const CALL: u8 = 0x42;
+
+/// Call A(A+2...A+D+MULTRES) and immediately return resulting values.
+/// ```no_run
+/// A: slot
+/// D: lit
+/// ```
+pub const CALLMT: u8 = 0x43;
+
+/// Call A(A+2...A+D-1) and immediately return resulting values.
+/// ```no_run
+/// A: slot
+/// D: lit
+/// ```
+pub const CALLT: u8 = 0x44;
+
+// ----- Iterator and var-args handling operations -----
+
+/// Call an iterator function located at A-3 with parameters A-2 and A-1,
+/// placing the result into A,A+1...A+B-2.
+/// This instruction perform a copy of A-3, A-2 and A-1 respectively into slots
+/// A, A+1 and A+2 to preserve iteration information.
+/// C is always 3 because an iteration function always accepts 2 parameters.
+/// ```no_run
+/// A: slot
+/// B: lit
+/// C: lit
+/// ```
+pub const ITERC: u8 = 0x45;
+
+/// Specialized version of the [`ITERC`] instruction, if the iteration function
+/// is `next`.
+pub const ITERN: u8 = 0x46;
+
+/// Set A,A+1...A+B-2 to variable arguments. C holds the number of fixed
+/// arguments of the enclosing function.
+/// ```no_run
+/// A: slot
+/// B: lit
+/// C: lit
+/// ```
+pub const VARG: u8 = 0x47;
+
+/// Runtime check to ensure the iteration function is `next`, if not, the loop
+/// is un-specialized into generic [`ITERC`] and [`JMP`].
+pub const ISNEXT: u8 = 0x48;
+
+// ----- Return operations -----
+
+/// Return A+A+1...A+D+MULTRES-1.
+/// ```no_run
+/// A: slot
+/// D: lit
+/// ```
+pub const RETM: u8 = 0x49;
+
+/// Return A+A+1...A+D-2.
+/// ```no_run
+/// A: rslot
+/// D: lit
+/// ```
+pub const RET: u8 = 0x4A;
+
+/// Return nothing.
+/// ```no_run
+/// A: rslot
+/// D: lit
+/// ```
+pub const RET0: u8 = 0x4B;
+
+/// Return A.
+/// ```no_run
+/// A: rslot
+/// D: lit
+/// ```
+pub const RET1: u8 = 0x4C;
+
+// ----- Numeric loop operations -----
+
+/// Numeric loop initialization, A is the base slot from which loop information
+/// are contained:
+///   * A -> The loop init index
+///   * A+1 -> The loop stop bound
+///   * A+2 -> The loop step
+///   * A+3 -> Is the loop current index, this slot is modified when calling
+///     a [`FORL`] instruction.
+/// D is just pointing to the instruction located immediately after the loop.
+/// ```no_run
+/// A: slot
+/// D: jump
+/// ```
+pub const FORI: u8 = 0x4D;
+
+/// JIT-compiled numeric loop initialization (do not use in code generation)
+pub const JFORI: u8 = 0x4E;
+
+/// Step forward in the loop execution:
+///   * Set A to A + A+2
+///   * Test if the loop is finished, if so just continue to the next
+///     instruction, else set A+3 to A and jump to D
+/// ```no_run
+/// A: slot
+/// D: jump
+/// ```
+pub const FORL: u8 = 0x4F;
+
+/// Interpreted numeric loop step forward (do not use in code generation).
+pub const IFORL: u8 = 0x50;
+
+/// JIT-compiled numeric loop step forward (do not use in code generation).
+pub const JFORL: u8 = 0x51;
+
+// ----- Iterator loop operations -----
+
+/// Check that the first result of an [`ITERC`] instruction is not `nil`, if so
+/// the result is copied to A-1 and jump to D, else just continue to the next
+/// instruction.
+pub const ITERL: u8 = 0x52;
+
+/// Interpreted iterator loop test (do not use in code generation).
+pub const IITERL: u8 = 0x53;
+
+/// JIT-compiled iterator loop test (do not use in code generation).
+pub const JITERL: u8 = 0x54;
+
+// ----- Generic loop operations -----
+
+/// Generic loop instruction, used by the engine for tracing purposes.
+pub const LOOP: u8 = 0x55;
+
+/// Generic loop instruction, used by the engine for tracing purposes.
+pub const ILOOP: u8 = 0x56;
+
+/// Generic loop instruction, used by the engine for tracing purposes.
+pub const JLOOP: u8 = 0x57;
+
+// ----- Simple branching -----
+
+/// Unconditionally branch to D, A is the next available slot at this stage.
+/// ```no_run
+/// A: rslot
+/// D: jump
+/// ```
+pub const JMP: u8 = 0x58;
+
+// ----- Function headers -----
+
+/// Declare a function with fixed arguments, used by the engine for tracing
+/// purposes.
+pub const FUNCF: u8 = 0x59;
+
+/// Declare a function with fixed arguments, force interpreter.
+pub const IFUNCF: u8 = 0x5A;
+
+/// Declare a function with fixed arguments, JIT-compiled.
+pub const JFUNCF: u8 = 0x5B;
+
+/// Declare a function with var-args, used by the engine for tracing purposes.
+pub const FUNCV: u8 = 0x5C;
+
+/// Declare a function with var-args, force interpreter.
+pub const IFUNCV: u8 = 0x5D;
+
+/// Declare a function with var-args, JIT-compiled.
+pub const JFUNCV: u8 = 0x5E;
+
+/// Declare a C function, used by the engine for tracing purposes.
+pub const FUNCC: u8 = 0x5F;
+
+/// Declare a C wrapped function, used by the engine for tracing purposes.
+pub const FUNCCW: u8 = 0x60;
