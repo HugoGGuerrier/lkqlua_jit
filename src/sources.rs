fe6a2a2abdb5226463424952e0d237527e7138e4
Hugo Guerrier <guerrier@adacore.com>
2025-06-11 18:25:18 +0200

    Add 'ariadne' as a dependency and start reworking the sources
    Source handling must be rehault to correctly integrate with the 'ariadne' library.

--- src/sources.rs
+++ src/sources.rs
@@ -7,9 +7,10 @@
 //! This module provide a [`SourceRepository`] type to store and cache analyzed
 //! sources.
 
-use std::{cell::OnceCell, collections::HashMap, fmt::Display, fs, path::Path};
+use std::{collections::HashMap, fmt::Display, fs, path::Path};
 
-use liblkqllang::{LkqlNode, SourceLocation};
+use ariadne::{Cache, Source};
+use liblkqllang::{AnalysisContext, AnalysisUnit, LkqlNode, SourceLocation};
 
 use crate::Error;
 
@@ -17,99 +18,114 @@ use crate::Error;
 /// holds all created sources, associating each one to its identifier.
 #[derive(Debug)]
 pub struct SourceRepository {
+    lkql_context: AnalysisContext,
     source_map: HashMap<SourceId, Source>,
+    lkql_unit_map: HashMap<SourceId, AnalysisUnit>,
 }
 
 impl SourceRepository {
-    /// Create a new empty source repository.
+    /// Create a new empty source repository with default config
+    /// (see https://github.com/HugoGGuerrier/lkqlua_jit/issues/2).
     pub fn new() -> Self {
-        Self { source_map: HashMap::new() }
+        Self {
+            lkql_context: AnalysisContext::create_default().unwrap(),
+            source_map: HashMap::new(),
+            lkql_unit_map: HashMap::new(),
+        }
     }
 
-    /// Get the source object for the provided file path, if it is already
-    /// stored in the [`self`] repository return a reference to it.
-    /// Otherwise, create a new source object and store it into the repository
-    /// before returning a referece to it.
-    /// This function returns an [`Error`] if the provided path is not
-    /// designating an existing file.
-    pub fn get_file_source(&mut self, file_path: &Path) -> Result<&Source, Error> {
-        let source_id = SourceId::from(file_path.canonicalize()?.to_str().unwrap());
-
-        // If it is not already present, insert the source in the map
-        if !self.source_map.contains_key(&source_id) {
-            self.source_map.insert(
-                source_id.clone(),
-                Source::File { canonical_path: source_id.clone(), content: OnceCell::new() },
-            );
-        }
-
-        Ok(self.source_map.get(&source_id).unwrap())
+    /// Register a new file designated by the provided path in the source
+    /// repository. If the operation is successful, then return the identifier
+    /// of the newly created source. Note that if the file has already been
+    /// loaded in this source repository and the `update` parameter is `true`,
+    /// then the file is loaded again and the repository is updated.
+    /// This method may fail if:
+    ///   * The provided file has already been loaded in this source repository
+    ///     and the `update` parameter is `false`
+    ///   * The provided path doesn't designate an existing file
+    ///   * The file designated by the provided file is not UTF-8 encoded
+    ///     (see https://github.com/HugoGGuerrier/lkqlua_jit/issues/1)
+    pub fn add_source_file(&mut self, file_path: &Path, update: bool) -> Result<SourceId, Error> {
+        let canonical_path = file_path.canonicalize()?;
+        let source_id = canonical_path.to_string_lossy().to_string();
+        self.add_source(source_id, || fs::read_to_string(&canonical_path), update)
     }
 
-    /// Get the source object associated to the provided buffer, if it is
-    /// already store in the [`self`] repository, return a reference to it.
-    /// Otherwise, create a new source object and store it into the repository
-    /// before returning a reference to it.
-    pub fn get_buffer_source(&mut self, name: &str, content: &str) -> &Source {
-        let source_id = SourceId::from(name);
+    /// Register the provided buffer in the source repository, returning the
+    /// identifier of the newly created source. If the `update` parameter is
+    /// `true`, the source is updated if it is already presents in the
+    /// repository.
+    /// This method may fail if:
+    ///   * The provided file has already been loaded in this source repository
+    ///     and the `update` parameter is `false`
+    pub fn add_source_buffer(
+        &mut self,
+        name: &str,
+        content: &str,
+        update: bool,
+    ) -> Result<SourceId, Error> {
+        let source_id = String::from(name);
+        self.add_source(source_id, || Ok::<String, Error>(String::from(content)), update)
+    }
 
-        // If it is not already present, insert the source in the map
-        if !self.source_map.contains_key(&source_id) {
-            self.source_map.insert(
-                source_id.clone(),
-                Source::Buffer { name: source_id.clone(), content: String::from(content) },
-            );
+    /// Internal function for adding sources.
+    fn add_source<F, E>(
+        &mut self,
+        id: SourceId,
+        content_provider: F,
+        update: bool,
+    ) -> Result<SourceId, Error>
+    where
+        F: Fn() -> Result<String, E>,
+        Error: From<E>,
+    {
+        if self.source_map.contains_key(&id) && !update {
+            return Err(Error::Messaged(format!(
+                "File \"{id}\" is already in the source repository"
+            )));
         }
+        self.source_map
+            .insert(id.clone(), Source::from(content_provider()?));
+        Ok(id)
+    }
 
-        self.source_map.get(&source_id).unwrap()
+    /// Parse the source designated by the provided identifier using the LKQL
+    /// parsing library. If the parsing succeeds, or if the source has already
+    /// been successfully parsed as LKQL, this function return the resulting
+    /// analysis unit.
+    /// This method may fail if:
+    ///   * An exception occurs in the parsing library
+    ///   * The source designated by the provided identifier is not a valid
+    ///     LKQL source
+    pub fn parse_as_lkql(
+        &mut self,
+        source: &SourceId,
+        reparse: bool,
+    ) -> Result<AnalysisUnit, Error> {
+        todo!()
+    }
+}
+
+impl Cache<SourceId> for SourceRepository {
+    type Storage = String;
+
+    fn fetch(&mut self, id: &SourceId) -> Result<&Source<Self::Storage>, impl std::fmt::Debug> {
+        self.source_map
+            .get(id)
+            .map_or(Err(Error::Messaged(format!("No source identified by \"{id}\""))), |s| {
+                Ok(s)
+            })
+    }
+
+    fn display<'a>(&self, id: &'a SourceId) -> Option<impl std::fmt::Display + 'a> {
+        Some(id)
     }
 }
 
 /// A source identifier is just a string.
 type SourceId = String;
 
-/// This enumeration is an abstraction over the source concepts. It can
-/// represents either a source file located on the disk, or a source buffer.
-/// It is not meant to be directly created, you should use a
-/// [`SourceRepository`] instance to create new source objects.
-/// For now only the UTF-8 sources are handled
-/// (see https://github.com/HugoGGuerrier/lkqlua_jit/issues/1).
-#[derive(Debug, Eq)]
-pub enum Source {
-    File { canonical_path: String, content: OnceCell<String> },
-    Buffer { name: String, content: String },
-}
-
-/// Sources are defined to be equal if they have the same [`fn@Self::id`],
-/// meaning that a [`Source::Buffer`] may be equals to a [`Source::File`].
-impl PartialEq for Source {
-    fn eq(&self, other: &Self) -> bool {
-        self.id() == other.id()
-    }
-}
-
-impl Source {
-    pub fn id(&self) -> &SourceId {
-        match self {
-            Source::File { canonical_path, content: _ } => canonical_path,
-            Source::Buffer { name, content: _ } => name,
-        }
-    }
-
-    pub fn content(&self) -> &String {
-        match self {
-            Source::File { canonical_path, content } => content.get_or_init(|| {
-                fs::read_to_string(canonical_path).expect(
-                    format!("An error occured while reading the \"{canonical_path}\" file")
-                        .as_str(),
-                )
-            }),
-            Source::Buffer { name: _, content } => content,
-        }
-    }
-}
-
-/// This structure represents an extract from a [`Source`].
+/// This structure represents an extract from a source object.
 #[derive(Debug, Clone, PartialEq, Eq)]
 pub struct SourceSection {
     pub source: SourceId,
@@ -124,7 +140,7 @@ impl Display for SourceSection {
 }
 
 impl SourceSection {
-    /// Create a [`SourceSection`] object from a [`liblkqllang`] node.
+    /// Create a new source section from a [`liblkqllang::LkqlNode`] object.
     pub fn from_lkql_node(node: &LkqlNode) -> Result<Self, Error> {
         let sloc_range = node.sloc_range()?;
         Ok(Self {
@@ -144,6 +160,7 @@ pub struct Location {
 }
 
 impl Location {
+    /// Create a new location from a [`liblkqllang::SourceLocation`] object.
     pub fn from_lkql_location(location: SourceLocation) -> Self {
         Self { line: location.line as u64, col: location.column as u32 }
     }
