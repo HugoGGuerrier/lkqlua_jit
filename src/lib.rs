5f5bd6cd8705294537e9967c4837283b54b1a71d
Hugo Guerrier <guerrier@adacore.com>
2025-06-17 15:14:59 +0200

    Create a first (very simple) version if the CLI

--- src/lib.rs
+++ src/lib.rs
@@ -3,7 +3,93 @@
 //! The library entry point of the LKQL engine, here you can find all required
 //! stuff to parse, compile and execute LKQL sources.
 
+use std::{collections::HashSet, io::Write, path::Path};
+
+use clap::ValueEnum;
+
+use crate::{intermediate_tree::Program, report::Report, sources::SourceRepository};
+
 pub mod bytecode;
 pub mod intermediate_tree;
 pub mod report;
 pub mod sources;
+
+/// This type holds all required data to run LKQL sources using LuaJIT as a
+/// backend. This is what you have to use.
+pub struct ExecutionContext<O: Write, E: Write> {
+    source_repo: SourceRepository,
+    config: EngineConfig<O, E>,
+}
+
+impl<O: Write, E: Write> ExecutionContext<O, E> {
+    /// Create an initialize a new execution context.
+    pub fn new(config: EngineConfig<O, E>) -> Self {
+        Self { source_repo: SourceRepository::new(), config }
+    }
+
+    /// Just run the provided LKQL file, don't return anything and report all
+    /// diagnosis and messages in the [`EngineConfig::std_err`] of the related
+    /// configuration.
+    pub fn just_run_lkql_file(&mut self, file: &Path) {
+        // Execute the file and get the result
+        let exec_res = self.execute_lkql_file(file);
+
+        // If there are errors, display them on STDERR
+        if let Err(report) = exec_res {
+            report.print(&self.source_repo, &mut self.config.std_err, false);
+        }
+    }
+
+    /// Execute the provided LKQL file, returning possible [`Report`] if the
+    /// execution is not successful.
+    pub fn execute_lkql_file(&mut self, file: &Path) -> Result<(), Report> {
+        // Add the source file to the source repo updating it if required
+        let source = self.source_repo.add_source_file(file, true)?;
+
+        // Parse the source file
+        let unit = self.source_repo.parse_as_lkql(&source)?;
+        let root = unit.root()?.unwrap();
+
+        // If required display the parsing tree
+        if self.config.is_verbose(VerboseElement::ParsingTree) {
+            writeln!(self.config.std_out, "===== Parsing tree =====\n")?;
+            writeln!(self.config.std_out, "{}\n", root.tree_dump(0)?)?;
+        }
+
+        // Lower the parsing tree
+        let lowering_tree = Program::lower_lkql_node(&root)?;
+
+        // If required display the lowered tree
+        if self.config.is_verbose(VerboseElement::LoweringTree) {
+            writeln!(self.config.std_out, "===== Lowering tree =====\n")?;
+            writeln!(self.config.std_out, "{}\n", lowering_tree)?;
+        }
+
+        // TODO: Compile the lowering tree and execute it
+        writeln!(self.config.std_out, "TODO: Compile the lowering tree and execute it")?;
+        Ok(())
+    }
+}
+
+#[derive(Debug)]
+pub struct EngineConfig<O: Write, E: Write> {
+    pub std_out: O,
+    pub std_err: E,
+    pub verbose_elements: HashSet<VerboseElement>,
+}
+
+impl<O: Write, E: Write> EngineConfig<O, E> {
+    pub fn is_verbose(&self, element: VerboseElement) -> bool {
+        self.verbose_elements.contains(&element)
+            || self.verbose_elements.contains(&VerboseElement::All)
+    }
+}
+
+/// This enum contains all elements that can be added to a "classic" output
+/// during a run of the engine.
+#[derive(Debug, PartialEq, Eq, Clone, Copy, Hash, ValueEnum)]
+pub enum VerboseElement {
+    All,
+    ParsingTree,
+    LoweringTree,
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,37 @@
+use std::{
+    io::{stderr, stdout},
+    path::PathBuf,
+};
+
+use clap::Parser;
+use lkqlua_jit::{EngineConfig, ExecutionContext, VerboseElement};
+
+#[derive(Parser, Debug)]
+#[command(about)]
+struct LauncherArgs {
+    #[arg(long, short = 'S', help = "LKQL file to run", value_name = "FILE")]
+    script: Option<PathBuf>,
+
+    #[arg(long, short, help = "Display additional information during the run")]
+    verbose: Vec<VerboseElement>,
+}
+
 fn main() {
-    println!("Hello, world!");
+    // Parse CLI arguments
+    let args = LauncherArgs::parse();
+
+    // Turn CLI arguments into an engine configuration
+    let config = EngineConfig {
+        std_out: stdout(),
+        std_err: stderr(),
+        verbose_elements: args.verbose.into_iter().collect(),
+    };
+
+    // Create a new LKQL execution context
+    let mut ctx = ExecutionContext::new(config);
+
+    // Run the provided script if any
+    if let Some(ref script) = args.script {
+        ctx.just_run_lkql_file(&script);
+    }
 }
