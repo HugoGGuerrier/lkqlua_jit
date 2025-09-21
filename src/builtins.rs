cfaa2373b85e6a55bbfbf871290593e698148853
Hugo Guerrier <guerrier@adacore.com>
2025-09-21 16:26:20 +0200

    Add the 'img' built-in function

--- src/builtins.rs
+++ src/builtins.rs
@@ -5,7 +5,10 @@
 
 use std::ffi::c_int;
 
-use crate::lua::{LuaCFunction, LuaState, to_string};
+use crate::lua::{LuaCFunction, LuaState, push_string, to_string};
+
+/// The default image of a value when the latter doesn't define one.
+pub const DEFAULT_IMG: &'static str = "<lkql_value>";
 
 /// This type encapsulate a built-in LKQL function. It contains all required
 /// information for the compilation and the execution.
@@ -20,12 +23,19 @@ pub fn get_builtins() -> Vec<BuiltinFunction> {
     fn b(name: &'static str, c_function: LuaCFunction) -> BuiltinFunction {
         BuiltinFunction { name, c_function }
     }
-    vec![b("print", lkql_print)]
+    vec![b("print", lkql_print), b("img", lkql_img)]
 }
 
 /// The "print" function
 #[unsafe(no_mangle)]
 unsafe extern "C" fn lkql_print(l: LuaState) -> c_int {
-    println!("{}", to_string(l, -1, "<lkql_value>"));
+    println!("{}", to_string(l, -1, DEFAULT_IMG));
     0
+}
+
+/// The "img" function
+#[unsafe(no_mangle)]
+unsafe extern "C" fn lkql_img(l: LuaState) -> c_int {
+    push_string(l, to_string(l, -1, DEFAULT_IMG));
+    1
 }
