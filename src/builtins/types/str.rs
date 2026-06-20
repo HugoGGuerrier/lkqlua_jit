44481365dc75143fedd2391670184a764c88e5c1
Hugo Guerrier <guerrier@adacore.com>
2026-06-21 01:29:20 +0200

    Add 'contains' and 'find' methods on strings

--- src/builtins/types/str.rs
+++ src/builtins/types/str.rs
@@ -6,16 +6,19 @@ use crate::{
     builtins::{
         traits::{self, sized::DEFAULT_SIZED_LENGTH},
         types::{
-            BuiltinType, TypeField, TypeImplementation, TypeImplementationKind, TypeRef,
-            img_property, int,
+            BuiltinType, TYPE_TAGS_FIELD, TypeField, TypeImplementation, TypeImplementationKind,
+            TypeRef, img_property, int,
         },
     },
     lua::{LuaState, copy_value, push_string, set_metatable},
     runtime::{Function, LkqlParam, RuntimeValue},
 };
+use const_format::formatcp;
+
+const TYPE_TAG: i32 = int::TYPE.tag + 1;
 
 pub const TYPE: BuiltinType = BuiltinType {
-    tag: int::TYPE.tag + 1,
+    tag: TYPE_TAG,
     traits: &[&traits::sized::TRAIT],
     implementation_variant: TypeImplementationKind::new_mono(IMPLEMENTATION),
 };
@@ -26,9 +29,11 @@ pub const IMPLEMENTATION: TypeImplementation = TypeImplementation {
         ("img", TypeField::Property(Function::CFunction(img_property))),
         ("length", TypeField::Property(DEFAULT_SIZED_LENGTH)),
         ("base_name", TypeField::Property(BASE_NAME)),
-        ("starts_with", TypeField::Value(RuntimeValue::Callable(STARTS_WITH))),
-        ("ends_with", TypeField::Value(RuntimeValue::Callable(ENDS_WITH))),
-        ("substring", TypeField::Value(RuntimeValue::Callable(SUBSTRING))),
+        ("starts_with", TypeField::Value(STARTS_WITH)),
+        ("ends_with", TypeField::Value(ENDS_WITH)),
+        ("contains", TypeField::Value(CONTAINS)),
+        ("find", TypeField::Value(FIND)),
+        ("substring", TypeField::Value(SUBSTRING)),
     ],
     overloads: &[],
     index_method: None,
@@ -57,29 +62,60 @@ const BASE_NAME: Function = Function::LuaFunction(
 );
 
 /// Implementation of the "starts_with" method.
-const STARTS_WITH: Function = Function::LkqlFunction {
+const STARTS_WITH: RuntimeValue = RuntimeValue::Callable(Function::LkqlFunction {
     params: &[
         LkqlParam::new("self"),
         LkqlParam::with_type("prefix", TypeRef::Str),
     ],
     body: "return string.sub(self, 1, #prefix) == prefix",
-};
+});
 
 /// Implementation of the "ends_with" method.
-const ENDS_WITH: Function = Function::LkqlFunction {
+const ENDS_WITH: RuntimeValue = RuntimeValue::Callable(Function::LkqlFunction {
     params: &[
         LkqlParam::new("self"),
         LkqlParam::with_type("suffix", TypeRef::Str),
     ],
     body: "return string.sub(self, #self - #suffix + 1, #self) == suffix",
-};
+});
+
+/// Implementation of the "contains" method.
+const CONTAINS: RuntimeValue = RuntimeValue::Callable(Function::LkqlFunction {
+    params: &[
+        LkqlParam::new("self"),
+        LkqlParam::with_types("to_find", &[TypeRef::Str, TypeRef::Pattern]),
+    ],
+    body: formatcp!(
+        "if to_find['{TYPE_TAGS_FIELD}'][{TYPE_TAG}] then
+            return string.find(self, to_find) ~= nil
+        else
+            return to_find.is_match(nil, to_find, self)
+        end",
+    ),
+});
+
+/// Implementation of the "find" method.
+const FIND: RuntimeValue = RuntimeValue::Callable(Function::LkqlFunction {
+    params: &[
+        LkqlParam::new("self"),
+        LkqlParam::with_types("to_find", &[TypeRef::Str, TypeRef::Pattern]),
+    ],
+    body: formatcp!(
+        "if to_find['{TYPE_TAGS_FIELD}'][{TYPE_TAG}] then
+            local res = string.find(self, to_find)
+            return res or -1
+        else
+            return to_find.find(nil, to_find, self)
+        end",
+    ),
+});
 
 /// Implementation of the "substring" method.
-const SUBSTRING: Function = Function::LkqlFunction {
+const SUBSTRING: RuntimeValue = RuntimeValue::Callable(Function::LkqlFunction {
     params: &[
         LkqlParam::new("self"),
         LkqlParam::with_type("low", TypeRef::Int),
         LkqlParam::with_type("high", TypeRef::Int),
     ],
     body: "return string.sub(self, low, high)",
-};
+});
