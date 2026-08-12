d6a69f0600ca3d5f426f4e6b1bcaa893a76cc9d5
Hugo Guerrier <guerrier@adacore.com>
2026-08-12 20:46:48 +0200

    Remove dependency to 'libgnat' and 'libgnarl' during build
    Those aren't required.

--- build.rs
+++ build.rs
@@ -11,10 +11,6 @@ fn main() {
     println!("cargo:rustc-link-search=all=./luajit/src");
     println!("cargo:rustc-link-lib=static=luajit");
 
-    // Statically link the Ada runtime
-    println!("cargo:rustc-link-lib=static=gnat");
-    println!("cargo:rustc-link-lib=static=gnarl");
-
     // Statically link libgmp (required by Liblkqllang)
     println!("cargo:rustc-link-lib=static=gmp");
 }
